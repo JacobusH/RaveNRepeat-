b6f048e197746f7e63571fba1517948f14862eb5
Jacobus <jacobus.anthony.harding@gmail.com>
2017-11-19 17:52:44 -0800

    initial setup and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './pages/playground/playground.component';
+import { HeaderComponent } from './pages/components/shared/header/header.component';
+import { FooterComponent } from './pages/components/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+;
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'playground', component: PlaygroundComponent },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
